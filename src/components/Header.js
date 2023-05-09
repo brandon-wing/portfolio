3121d08b3e4e802ecaf6c40d90e8dc1388d25288
rudyxwhite <brandonrwing98@gmail.com>
2023-05-09 11:25:39 -0500

    added header and navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Navigation from './Navigation';
-
+import Background from '../images/header.png';
 
 const Header = () => {
   return (
     <div>
-<Navigation />
+      <div
+        className="p-5 text-center bg-image"
+        style={{ 
+          backgroundImage: `url(${Background})`, 
+          height: '350px', 
+          width: '100vw', 
+          backgroundSize: 'cover'
+        }}
+      >
+        <div className="mask">
+          <div className="d-flex justify-content-center align-items-center h-100">
+            <div className="text-white">
+            <h1 className="mb-3" style={{fontSize: '5rem'}}>BRANDON WING: DEVELOPER IN TRAINING</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Navigation />
     </div>
   );
 }
