12ca260c11ea794282ffbfe7bed923263b8a62ae
rudyxwhite <brandonrwing98@gmail.com>
2023-05-10 12:30:01 -0500

    base contact form created, no functionality yet

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import '../../components/styles.css';
 
 const Contact = () => {
   return (
-    <div>
-<h1>Contact Me</h1>
-    </div>
+    <Container className='specific-section'>
+      <Row className="mb-1 pt-3">
+        <Col>
+          <h1>Contact Me</h1>
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col md={6}>
+          <Form>
+            <Form.Group controlId="formName" className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name" />
+            </Form.Group>
+
+            <Form.Group controlId="formEmail" className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter your email address" />
+            </Form.Group>
+
+            <Form.Group controlId="formMessage" className="mb-3">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={3} placeholder="Enter your message here." />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
